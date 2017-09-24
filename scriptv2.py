a01d733aa99a378600d5488dad8533757146933d
Yingxiang <yingxiang@smarking.net>
2017-09-24 09:50:05 -0700

    grid search lgb param

--- scriptv2.py
+++ scriptv2.py
@@ -8,6 +8,7 @@ import lightgbm as lgb
 import gc
 from sklearn.linear_model import LinearRegression
 import random
+from sklearn.model_selection import GridSearchCV
 import datetime as dt
 
 ################
@@ -27,10 +28,10 @@ OLS_WEIGHT = 0.0828
 
 XGB1_WEIGHT = 0.8083  # Weight of first in combination of two XGB models
 
-BASELINE_PRED = 0.0115   # Baseline based on mean of training data, per Oleg
+BASELINE_PRED = 0.0115  # Baseline based on mean of training data, per Oleg
 
 ##### READ IN RAW DATA
-print( "\nReading data from disk ...")
+print("\nReading data from disk ...")
 prop = pd.read_csv('../input/properties_2016.csv')
 train = pd.read_csv("../input/train_2016_v2.csv")
 
@@ -42,12 +43,13 @@ def fill_missing_values(df):
     df.loc[index, 'taxamount'] = df[index]['taxvaluedollarcnt'] / 73
     # use taxvaluedollarcnt - landtaxvaluedollarcnt to impute structuretaxvaluedollarcnt
     index = df['taxvaluedollarcnt'].notnull() & df['landtaxvaluedollarcnt'].notnull() \
-    & df['structuretaxvaluedollarcnt'].isnull()
+            & df['structuretaxvaluedollarcnt'].isnull()
     df.loc[index, 'structuretaxvaluedollarcnt'] = df[index]['taxvaluedollarcnt'] - \
-    df[index]['landtaxvaluedollarcnt']
+                                                  df[index]['landtaxvaluedollarcnt']
     # Use mean value to fill the rest
     df = df.fillna(df.median())
     return df
+
 
 ################
 ################
@@ -56,26 +58,25 @@ def fill_missing_values(df):
 ################
 
 ##### PROCESS DATA FOR LIGHTGBM
-print( "\nProcessing data for LightGBM ..." )
-for c, dtype in zip(prop.columns, prop.dtypes):	
-    if dtype == np.float64:	
+print("\nProcessing data for LightGBM ...")
+for c, dtype in zip(prop.columns, prop.dtypes):
+    if dtype == np.float64:
         prop[c] = prop[c].astype(np.float32)
 
 df_train = train.merge(prop, how='left', on='parcelid')
 # df_train.fillna(df_train.median(), inplace = True)
 df_train = fill_missing_values(df_train)
 
-x_train = df_train.drop(['parcelid', 'logerror', 'transactiondate', 'propertyzoningdesc', 
+x_train = df_train.drop(['parcelid', 'logerror', 'transactiondate', 'propertyzoningdesc',
                          'propertycountylandusecode', 'fireplacecnt', 'fireplaceflag'], axis=1)
 # drop very sparse columns
 # x_train = x_train.drop(['storytypeid', 'basementsqft', 'yardbuildingsqft26', 'architecturalstyletypeid'], axis=1)
 
 
 
-#x_train['Ratio_1'] = x_train['taxvaluedollarcnt']/x_train['taxamount']
+# x_train['Ratio_1'] = x_train['taxvaluedollarcnt']/x_train['taxamount']
 y_train = df_train['logerror'].values
 print(x_train.shape, y_train.shape)
-
 
 train_columns = x_train.columns
 
@@ -84,35 +85,60 @@ for c in x_train.dtypes[x_train.dtypes == object].index.values:
     # taxdelinquencyflag
     x_train[c] = (x_train[c] == True)
 
-del df_train; gc.collect()
+del df_train;
+gc.collect()
 
 x_train = x_train.values.astype(np.float32, copy=False)
 d_train = lgb.Dataset(x_train, label=y_train)
 
-
-
 ##### RUN LIGHTGBM
 params = {}
 params['max_bin'] = 10
-params['learning_rate'] = 0.0021 # shrinkage_rate
+params['learning_rate'] = 0.0021  # shrinkage_rate
 params['boosting_type'] = 'gbdt'
 params['objective'] = 'regression'
-params['metric'] = 'l1'          # or 'mae'
-params['sub_feature'] = 0.345    
-params['bagging_fraction'] = 0.85 # sub_row
+params['metric'] = 'l1'  # or 'mae'
+params['sub_feature'] = 0.345
+params['bagging_fraction'] = 0.85  # sub_row
 params['bagging_freq'] = 40
-params['num_leaves'] = 512        # num_leaf
-params['min_data'] = 500         # min_data_in_leaf
-params['min_hessian'] = 0.05     # min_sum_hessian_in_leaf
+params['num_leaves'] = 512  # num_leaf
+params['min_data'] = 500  # min_data_in_leaf
+params['min_hessian'] = 0.05  # min_sum_hessian_in_leaf
 params['verbose'] = 0
 params['feature_fraction_seed'] = 2
 params['bagging_seed'] = 3
 
 print("\nFitting LightGBM model ...")
-clf = lgb.train(params, d_train, 430)
+# clf = lgb.train(params, d_train, 430)
+estimator = lgb.LGBMRegressor(num_leaves=512)
+param_grid = {
+    'max_bin': [10, 15],
+    'learning_rate': [0.0021, 0.0011, 0.02],
+    'boosting_type': ['gbdt'],
+    'objective': 'regression',
+    'metric': ['l1'],
+    'sub_feature': [0.345],
+    'bagging_fraction': [0.85],
+    'bagging_freq': [40],
+    'min_data': [500, 400],
+    'min_hessian': [0.05],
+    'verbose': [1],
+    'feature_fraction_seed': [2],
+    'bagging_seed': [3]
+}
+print('>>>> grid search')
+gbm = GridSearchCV(estimator, param_grid, verbose=1)
+gbm.fit(x_train, y_train)
+best_params = gbm.best_params_
+print('>>>> best_param')
+best_params['num_leaves'] = 512
+clf = lgb.train(best_params, d_train, 430)
 
-del d_train; gc.collect()
-del x_train; gc.collect()
+
+del d_train;
+gc.collect()
+del x_train;
+gc.collect()
 
 print("\nPrepare for LightGBM prediction ...")
 print("   Read sample file ...")
@@ -122,12 +148,14 @@ sample['parcelid'] = sample['ParcelId']
 print("   Merge with property data ...")
 df_test = sample.merge(prop, on='parcelid', how='left')
 print("   ...")
-del sample, prop; gc.collect()
+del sample, prop;
+gc.collect()
 print("   ...")
-#df_test['Ratio_1'] = df_test['taxvaluedollarcnt']/df_test['taxamount']
+# df_test['Ratio_1'] = df_test['taxvaluedollarcnt']/df_test['taxamount']
 x_test = df_test[train_columns]
 print("   ...")
-del df_test; gc.collect()
+del df_test;
+gc.collect()
 print("   Preparing x_test...")
 for c in x_test.dtypes[x_test.dtypes == object].index.values:
     x_test[c] = (x_test[c] == True)
@@ -138,13 +166,11 @@ print("Test shape :", x_test.shape)
 print("\nStart LightGBM prediction ...")
 p_test = clf.predict(x_test)
 
-del x_test; gc.collect()
+del x_test;
+gc.collect()
 
-print( "\nUnadjusted LightGBM predictions:" )
-print( pd.DataFrame(p_test).head() )
-
-
-
+print("\nUnadjusted LightGBM predictions:")
+print(pd.DataFrame(p_test).head())
 
 ################
 ################
@@ -155,35 +181,32 @@ print( pd.DataFrame(p_test).head() )
 ##### RE-READ PROPERTIES FILE
 ##### (I tried keeping a copy, but the program crashed.)
 
-print( "\nRe-reading properties file ...")
+print("\nRe-reading properties file ...")
 properties = pd.read_csv('../input/properties_2016.csv')
 
 ##### PROCESS DATA FOR XGBOOST
-print( "\nProcessing data for XGBoost ...")
+print("\nProcessing data for XGBoost ...")
 for c in properties.columns:
-    properties[c]=properties[c].fillna(-1)
-    if properties[c].dtype == 'object':
-        lbl = LabelEncoder()
-        lbl.fit(list(properties[c].values))
-        properties[c] = lbl.transform(list(properties[c].values))
+    properties[c] = properties[c].fillna(-1)
+if properties[c].dtype == 'object':
+    lbl = LabelEncoder()
+lbl.fit(list(properties[c].values))
+properties[c] = lbl.transform(list(properties[c].values))
 
 train_df = train.merge(properties, how='left', on='parcelid')
-x_train = train_df.drop(['parcelid', 'logerror','transactiondate'], axis=1)
+x_train = train_df.drop(['parcelid', 'logerror', 'transactiondate'], axis=1)
 x_test = properties.drop(['parcelid'], axis=1)
 # shape        
 print('Shape train: {}\nShape test: {}'.format(x_train.shape, x_test.shape))
 
 # drop out ouliers
-train_df=train_df[ train_df.logerror > -0.4 ]
-train_df=train_df[ train_df.logerror < 0.419 ]
-x_train=train_df.drop(['parcelid', 'logerror','transactiondate'], axis=1)
+train_df = train_df[train_df.logerror > -0.4]
+train_df = train_df[train_df.logerror < 0.419]
+x_train = train_df.drop(['parcelid', 'logerror', 'transactiondate'], axis=1)
 y_train = train_df["logerror"].values.astype(np.float32)
 y_mean = np.mean(y_train)
-print('After removing outliers:')     
+print('After removing outliers:')
 print('Shape train: {}\nShape test: {}'.format(x_train.shape, x_test.shape))
-
-
-
 
 ##### RUN XGBOOST
 print("\nSetting up data for XGBoost ...")
@@ -194,8 +217,8 @@ xgb_params = {
     'subsample': 0.80,
     'objective': 'reg:linear',
     'eval_metric': 'mae',
-    'lambda': 0.8,   
-    'alpha': 0.4, 
+    'lambda': 0.8,
+    'alpha': 0.4,
     'base_score': y_mean,
     'silent': 1
 }
@@ -204,19 +227,17 @@ dtrain = xgb.DMatrix(x_train, y_train)
 dtest = xgb.DMatrix(x_test)
 
 num_boost_rounds = 250
-print("num_boost_rounds="+str(num_boost_rounds))
+print("num_boost_rounds=" + str(num_boost_rounds))
 
 # train model
-print( "\nTraining XGBoost ...")
+print("\nTraining XGBoost ...")
 model = xgb.train(dict(xgb_params, silent=1), dtrain, num_boost_round=num_boost_rounds)
 
-print( "\nPredicting with XGBoost ...")
+print("\nPredicting with XGBoost ...")
 xgb_pred1 = model.predict(dtest)
 
-print( "\nFirst XGBoost predictions:" )
-print( pd.DataFrame(xgb_pred1).head() )
-
-
+print("\nFirst XGBoost predictions:")
+print(pd.DataFrame(xgb_pred1).head())
 
 ##### RUN XGBOOST AGAIN
 print("\nSetting up data for XGBoost ...")
@@ -232,26 +253,24 @@ xgb_params = {
 }
 
 num_boost_rounds = 150
-print("num_boost_rounds="+str(num_boost_rounds))
+print("num_boost_rounds=" + str(num_boost_rounds))
 
-print( "\nTraining XGBoost again ...")
+print("\nTraining XGBoost again ...")
 model = xgb.train(dict(xgb_params, silent=1), dtrain, num_boost_round=num_boost_rounds)
 
-print( "\nPredicting with XGBoost again ...")
+print("\nPredicting with XGBoost again ...")
 xgb_pred2 = model.predict(dtest)
 
-print( "\nSecond XGBoost predictions:" )
-print( pd.DataFrame(xgb_pred2).head() )
-
-
+print("\nSecond XGBoost predictions:")
+print(pd.DataFrame(xgb_pred2).head())
 
 ##### COMBINE XGBOOST RESULTS
 
-xgb_pred = XGB1_WEIGHT*xgb_pred1 + (1-XGB1_WEIGHT)*xgb_pred2
-#xgb_pred = xgb_pred1
+xgb_pred = XGB1_WEIGHT * xgb_pred1 + (1 - XGB1_WEIGHT) * xgb_pred2
+# xgb_pred = xgb_pred1
 
-print( "\nCombined XGBoost predictions:" )
-print( pd.DataFrame(xgb_pred).head() )
+print("\nCombined XGBoost predictions:")
+print(pd.DataFrame(xgb_pred).head())
 
 del train_df
 del x_train
@@ -260,10 +279,8 @@ del properties
 del dtest
 del dtrain
 del xgb_pred1
-del xgb_pred2 
+del xgb_pred2
 gc.collect()
-
-
 
 ################
 ################
@@ -281,7 +298,8 @@ random.seed(17)
 train = pd.read_csv("../input/train_2016_v2.csv", parse_dates=["transactiondate"])
 properties = pd.read_csv("../input/properties_2016.csv")
 submission = pd.read_csv("../input/sample_submission.csv")
-print(len(train),len(properties),len(submission))
+print(len(train), len(properties), len(submission))
+
 
 def get_features(df):
     df["transactiondate"] = pd.to_datetime(df["transactiondate"])
@@ -291,33 +309,33 @@ def get_features(df):
     df = df.fillna(-1.0)
     return df
 
+
 def MAE(y, ypred):
-    #logerror=log(Zestimate)−log(SalePrice)
-    return np.sum([abs(y[i]-ypred[i]) for i in range(len(y))]) / len(y)
+    # logerror=log(Zestimate)−log(SalePrice)
+    return np.sum([abs(y[i] - ypred[i]) for i in range(len(y))]) / len(y)
+
 
 train = pd.merge(train, properties, how='left', on='parcelid')
 y = train['logerror'].values
 test = pd.merge(submission, properties, how='left', left_on='ParcelId', right_on='parcelid')
-properties = [] #memory
+properties = []  # memory
 
-exc = [train.columns[c] for c in range(len(train.columns)) if train.dtypes[c] == 'O'] + ['logerror','parcelid']
+exc = [train.columns[c] for c in range(len(train.columns)) if train.dtypes[c] == 'O'] + ['logerror', 'parcelid']
 col = [c for c in train.columns if c not in exc]
 
 train = get_features(train[col])
-test['transactiondate'] = '2016-01-01' #should use the most common training date
+test['transactiondate'] = '2016-01-01'  # should use the most common training date
 test = get_features(test[col])
 
-
 reg = LinearRegression(n_jobs=-1)
-reg.fit(train, y); print('fit...')
+reg.fit(train, y);
+print('fit...')
 print(MAE(y, reg.predict(train)))
-train = [];  y = [] #memory
+train = [];
+y = []  # memory
 
-test_dates = ['2016-10-01','2016-11-01','2016-12-01','2017-10-01','2017-11-01','2017-12-01']
-test_columns = ['201610','201611','201612','201710','201711','201712']
-
-
-
+test_dates = ['2016-10-01', '2016-11-01', '2016-12-01', '2017-10-01', '2017-11-01', '2017-12-01']
+test_columns = ['201610', '201611', '201612', '201710', '201711', '201712']
 
 ########################
 ########################
@@ -326,29 +344,28 @@ test_columns = ['201610','201611','201612','201710','201711','201712']
 ########################
 
 ##### COMBINE PREDICTIONS
-print( "\nCombining XGBoost, LightGBM, and baseline predicitons ..." )
+print("\nCombining XGBoost, LightGBM, and baseline predicitons ...")
 lgb_weight = (1 - XGB_WEIGHT - BASELINE_WEIGHT) / (1 - OLS_WEIGHT)
 xgb_weight0 = XGB_WEIGHT / (1 - OLS_WEIGHT)
-baseline_weight0 =  BASELINE_WEIGHT / (1 - OLS_WEIGHT)
-pred0 = xgb_weight0*xgb_pred + baseline_weight0*BASELINE_PRED + lgb_weight*p_test
+baseline_weight0 = BASELINE_WEIGHT / (1 - OLS_WEIGHT)
+pred0 = xgb_weight0 * xgb_pred + baseline_weight0 * BASELINE_PRED + lgb_weight * p_test
 
-print( "\nCombined XGB/LGB/baseline predictions:" )
-print( pd.DataFrame(pred0).head() )
+print("\nCombined XGB/LGB/baseline predictions:")
+print(pd.DataFrame(pred0).head())
 
-print( "\nPredicting with OLS and combining with XGB/LGB/baseline predicitons: ..." )
+print("\nPredicting with OLS and combining with XGB/LGB/baseline predicitons: ...")
 for i in range(len(test_dates)):
     test['transactiondate'] = test_dates[i]
-    pred = OLS_WEIGHT*reg.predict(get_features(test)) + (1-OLS_WEIGHT)*pred0
+    pred = OLS_WEIGHT * reg.predict(get_features(test)) + (1 - OLS_WEIGHT) * pred0
     submission[test_columns[i]] = [float(format(x, '.4f')) for x in pred]
     print('predict...', i)
 
-print( "\nCombined XGB/LGB/baseline/OLS predictions:" )
-print( submission.head() )
-
-
+print("\nCombined XGB/LGB/baseline/OLS predictions:")
+print(submission.head())
 
 ##### WRITE THE RESULTS
 from datetime import datetime
-print( "\nWriting results to disk ..." )
+
+print("\nWriting results to disk ...")
 submission.to_csv('sub{}.csv'.format(datetime.now().strftime('%Y%m%d_%H%M%S')), index=False)
-print( "\nFinished ...")
+print("\nFinished ...")
